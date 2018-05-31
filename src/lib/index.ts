b809cc6ec8811c5aec6ce86f6ea269e634e8d34a
stefanFCB94 <stefan.laeufle@gmail.com>
2018-05-31 12:37:32 +0200

    Removed unused code

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,26 +2,6 @@ import { container } from './inversify.config';
 import { TYPES } from './types';
 import { IServer } from './interfaces/IServer';
 
-// const schemaTypes = importSchema(__dirname + '/api/schema/schema.graphql');
-// const schema = makeExecutableSchema({ resolvers, typeDefs: schemaTypes });
-
-// const GRAPHQL_PORT = 3000;
-// const server = express();
-
-// server.use('/graphql', bodyParser.json(), graphqlExpress({ schema, formatError }));
-// server.use('/graphiql',  graphiqlExpress({ endpointURL: '/graphql' }));
-
-
-// server.listen(GRAPHQL_PORT, () => {
-//   console.log('Server is now listening');
-
-//   process.on('SIGINT', async () => {
-//     const dbService = container.get<IDatabaseService>(TYPES.DatabaseService);
-//     await dbService.closeConnection();
-
-//     process.exit(0);
-//   });
-// });
 
 async function start() {
   const server = container.get<IServer>(TYPES.Server);
