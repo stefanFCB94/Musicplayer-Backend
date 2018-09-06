4a1895e13b3be86d8352870997908feec7c12398
stefanFCB94 <stefan.laeufle@gmail.com>
2018-09-06 22:37:54 +0200

    Added error, if format is not transmitted

--- src/lib/api/rest/routes/preferences/putImageFormat.ts
+++ src/lib/api/rest/routes/preferences/putImageFormat.ts
@@ -5,14 +5,20 @@ import { TYPES } from '../../../../types';
 
 import { IImageProcessingService } from '../../../../interfaces/services/IImageProcessingService';
 import { sendError } from '../../utils/sendError';
+import { RequestParameterNotSetError } from '../../../../error/request/RequestParameterNotSetError';
+import { ILogger } from '../../../../interfaces/services/ILogger';
 
 
 export async function putImageFormat(req: express.Request, res: express.Response) {
   const imageProcessing = container.get<IImageProcessingService>(TYPES.ImageProcessingService);
+  const logger = container.get<ILogger>(TYPES.Logger);
 
   try {
     if (!req.body || typeof req.body.format === 'undefined') {
+      const error = new RequestParameterNotSetError('format', 'Format must be set');
+      logger.log(error.stack, 'warn');
 
+      throw error;
     }
 
     const format: string = req.body.format;
