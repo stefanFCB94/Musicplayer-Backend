2ea1b2f15c7df8a0d9387861da7c18f038f817b1
stefanFCB94 <stefan.laeufle@gmail.com>
2018-09-20 19:02:43 +0200

    Updated storage service to use system preference service

--- src/lib/interfaces/services/IStorageService.ts
+++ src/lib/interfaces/services/IStorageService.ts
@@ -1,4 +1,7 @@
 export interface IStorageService {
+  getBaseStorage(): Promise<string>;
+  setBaseStorage(path: string, opts?: { moveContent?: boolean }): Promise<void>;
+  
   saveFile(path: string, filename: string, file: Buffer): Promise<string>;
   deleteFile(path: string): Promise<void>;
   exists(path: string): Promise<boolean>;
--- src/lib/services/StorageService.ts
+++ src/lib/services/StorageService.ts
@@ -2,13 +2,13 @@ import * as fs from 'fs-extra';
 import * as mkdirp from 'mkdirp';
 
 import { IStorageService } from '../interfaces/services/IStorageService';
-import { BaseConfigService } from '../base/BaseConfigService';
+import { BaseSystemPreferenceService } from '../base/BaseSystemPreferenceService';
 
 import { TYPES } from '../types';
 import { inject } from 'inversify';
 
 import { ILogger } from '../interfaces/services/ILogger';
-import { IConfigServiceProvider } from '../interfaces/services/IConfigService';
+import { ISystemPreferencesService } from '../interfaces/services/ISystemPreferencesService';
 
 import { RequiredConfigParameterNotSetError } from '../error/config/RequiredConfigParamterNotSetError';
 import { StorageNotExistsError } from '../error/server/StorageNotExistsError';
@@ -18,68 +18,73 @@ import { StorageFileNotExistingError } from '../error/server/StorageFileNotExist
 import { StorageFileNotDeletableError } from '../error/server/StorageFileNotDeletableError';
 
 
-export class StorageService extends BaseConfigService implements IStorageService {
+export class StorageService extends BaseSystemPreferenceService implements IStorageService {
 
-  private serviceInitialized = false;
-
-
-  private baseStorage: string;
-  
   private baseStorageKey: string = 'STORAGE.PATH';
 
 
   constructor(
     @inject(TYPES.Logger) logger: ILogger,
-    @inject(TYPES.ConfigServiceProvider) configProvider: IConfigServiceProvider,
+    @inject(TYPES.SystemPreferencesService) systemPreferenceService: ISystemPreferencesService,
   ) {
-    super(logger, configProvider);
+    super(logger, systemPreferenceService);
   }
 
   /**
-   * Initialize the storage service, so that it can be used.
-   * Reads the path, where the files of the application should
-   * be stored and checks, if that directory exists and can be
-   * writen into.
-   * 
    * @private
+   * @author Stefan Läufle
    * 
-   * @throws RequiredConfigParameterNotSetError
-   * @throws StorageNotExistsError
-   * @throws StorageNotWritableError
+   * Checks if the storage exists.
+   * Path to be checked must be given as parameter.
+   * 
+   * Storage is not read from the system preference service
+   * for performance reasons.
+   * 
+   * @param {string} path The path of the storage
+   * @returns {Promise<boolean>} true
+   * 
+   * @throws {StorageNotExistsError}
    */
-  private async init() {
-    if (this.serviceInitialized) { return; }
+  private async existsStorage(path: string): Promise<boolean> {
+    try {
+      const stats = await fs.stat(path);
 
-    this.logger.log('Starting intialisation for the storage service', 'debug');
-    await this.initConfigService();
-
-    // Read the path of the base storage from the configuration file
-    this.baseStorage = this.configService.get(this.baseStorageKey);
-    if (!this.baseStorage || typeof this.baseStorage !== 'string') {
-      const error = new RequiredConfigParameterNotSetError(this.baseStorageKey, 'Path to diretory, where to store files, must be set');
-      this.logger.log(error.stack, 'error');
-      throw error;
+      if (!stats.isFile()) {
+        return true;
+      }
+    } catch (err) {
+      this.logger.log(err.stack, 'error');
     }
 
-    // Check if base storage exists in the file system
-    const storageExists = await fs.pathExists(this.baseStorage);
-    const stats = await fs.stat(this.baseStorage);
-    if (!storageExists || stats.isFile()) {
-      const error = new StorageNotExistsError(this.baseStorage, 'Path does not exists on the filesystem');
-      this.logger.log(error.stack, 'error');
-      throw error;
-    }
+    const error = new StorageNotExistsError(path, 'Path does not exists on the filesystem');
+    this.logger.log(error.stack, 'error');
+    throw error;
+  }
 
+  /**
+   * @private
+   * @author Stefan Läufle
+   * 
+   * Checks if a path is writable.
+   * Works only on Linux and Mac systems, on Windows
+   * the API for the user access rights is not good enough.
+   * 
+   * @param {string} path Full path to be checked
+   * @returns {Promise<boolean>} true
+   *  
+   * @throws {StorageNotWritableError} If the path is not writable
+   */
+  private async isWritable(path: string): Promise<boolean> {
     if (process.platform !== 'win32') {
-      const hasWrite = await fs.access(this.baseStorage, fs.constants.W_OK);
+      const hasWrite = await fs.access(path, fs.constants.W_OK);
       if (!hasWrite) {
-        const error = new StorageNotWritableError(this.baseStorage, 'Path is not writable');
+        const error = new StorageNotWritableError(path, 'Path is not writable');
         this.logger.log(error.stack, 'error');
         throw error;
       }
     }
 
-    this.logger.log('Storage service fully initialised', 'debug');
+    return true;
   }
 
 
@@ -100,13 +105,18 @@ export class StorageService extends BaseConfigService implements IStorageService
    * 
    * @returns {Promise<void>} Resolves, if directory exists or was successfully created
    *                          Rejects with the appeared error 
+   * 
+   * @throws {StoragePathNotCreatableError}
    */
   private async createSubdir(path: string): Promise<void> {
     return new Promise<void>((resolve, reject) => {
       mkdirp(path, (err) => {
         if (err) {
           this.logger.log(err.stack, 'error');
-          return reject(err);
+
+          const error = new StoragePathNotCreatableError(path, 'Path not creatable');
+          this.logger.log(error.stack, 'error');
+          throw error;
         }
 
         this.logger.log(`Path '${path}' created`, 'debug');
@@ -115,7 +125,81 @@ export class StorageService extends BaseConfigService implements IStorageService
     });
   }
 
-  
+
+  /**
+   * @public
+   * @author Stefan Läufle
+   * 
+   * Gets the configured path to the base storage from the system
+   * preference service. Returns null, if path is not defined
+   * 
+   * @returns {Promise<string>} The setted storage parameter
+   * 
+   * @throws {ServiceNotInitializedError}
+   * @throws {Error}
+   */
+  public async getBaseStorage(): Promise<string> {
+    const storage = await this.systemPreferenceService.getPreferenceValues(this.baseStorageKey);
+    
+    if (!storage || storage.length === 0){
+      return null;
+    }
+
+    return storage[0];
+  }
+
+  /**
+   * @public
+   * @author Stefan Läufle
+   * 
+   * Set a new base storage to the system prefernces.
+   * Default all contents of the old storage will be moved to the
+   * new storage.
+   * 
+   * @param {string} path The path to the new storage 
+   * @param {object} opts Options for the function
+   * @param {boolean} opts.moveContent Default true, if the content should be moved to new storage
+   * 
+   * @returns {Promise<void>}
+   * 
+   * @throws {ServiceNotInitializedError}
+   * @throws {StorageNotExistsError}
+   * @throws {StorageNotWritableError}
+   * @throws {ParameterOutOfBoundsError}
+   * @throws {Error}
+   */
+  public async setBaseStorage(path: string, opts?: { moveContent?: boolean }): Promise<void> {
+    this.logger.log('Set the base storage for the server', 'debug');
+
+    // CHeck, if new storage path exists
+    await this.existsStorage(path);
+
+    // Check, if new storage path is writable
+    await this.isWritable(path);
+
+    const oldPath = await this.getBaseStorage();
+    await this.systemPreferenceService.savePreference(this.baseStorageKey, [path]);
+
+    // If option moveContent is set to a true or is not defined value
+    // the service tries to move all contents from the old storage to
+    // the new one
+    if (oldPath && oldPath !== path && (!opts || typeof opts.moveContent === 'undefined' || opts.moveContent)) {
+      this.logger.log('Move all contents of the old storage to the new one', 'debug');
+      const content = await fs.readdir(oldPath);
+
+      content.forEach(async element => {
+        const source = oldPath + '/' + element;
+        const target = path + '/' + element;
+
+        await fs.move(source, target, { overwrite: true });
+        this.logger.log(`${element} moved to new storage`, 'debug');
+      });
+
+      this.logger.log('All content elements moved to new storage directory', 'debug');
+    }
+  }
+
+
   /**
    * Saves a file in the defined storage space.
    * As parameter the subdirectory of the storage, the filename for the file
@@ -131,36 +215,37 @@ export class StorageService extends BaseConfigService implements IStorageService
    * @param {string} filename The name of the file to be saved
    * @param {Buffer} file The file, which should be saved
    * 
-   * @throws RequiredConfigParameterNotSetError
-   * @throws StorageNotExistsError
-   * @throws StoragePathNotCreateableError
-   * @throws StorageNotWritableError
+   * @throws {ServiceNotInitializedError}
+   * @throws {RequiredConfigParameterNotSetError}
+   * @throws {StorageNotExistsError}
+   * @throws {StoragePathNotCreateableError}
+   * @throws {StorageNotWritableError}
+   * @throws {Error}
    */
-  async saveFile(path: string, filename: string, file: Buffer): Promise<string> {
-    await this.init();
+  public async saveFile(path: string, filename: string, file: Buffer): Promise<string> {
+    const baseStorage = await this.getBaseStorage();
+    if (!baseStorage) {
+      const error = new RequiredConfigParameterNotSetError(this.baseStorageKey, 'Storage must be defined');
+      this.logger.log('File could not be saved. Storage not defined', 'error');
+      this.logger.log(error.stack, 'error');
 
-    const pathToSaveIn = this.baseStorage + '/' + path;
+      throw error;
+    }
+
+    const pathToSaveIn = baseStorage + '/' + path;
     const exists = await this.exists(path);
 
     if (!exists) {
+      this.logger.log('Path, where the file should be created not available, check if base storage exists', 'debug');
+      await this.existsStorage(baseStorage);
+ 
       this.logger.log('Path has to be created', 'debug');
-      try {
-        await this.createSubdir(pathToSaveIn);
-      } catch (err) {
-        const error = new StoragePathNotCreatableError(pathToSaveIn, 'Path could not be created on the file system');
-        this.logger.log(error.stack, 'error');
-        throw error;
-      }
+      await this.createSubdir(pathToSaveIn);
     }
 
-    if (process.platform !== 'win32') {
-      const access = await fs.access(pathToSaveIn, fs.constants.W_OK);
-      if (!access) {
-        const error = new StorageNotWritableError(pathToSaveIn, 'In target directory cannot be writen');
-        this.logger.log(error.stack, 'error');
-        throw error;
-      }
-    }
+    // Check, if storage path is writable
+    // throws StorageNotWritableError
+    await this.isWritable(pathToSaveIn);
 
     this.logger.log('Path exists and can be writen in. Now write file to filesystem', 'debug');
     const filepath = pathToSaveIn + '/' + filename;
@@ -180,9 +265,9 @@ export class StorageService extends BaseConfigService implements IStorageService
   /**
    * Delete a file from the storage.
    * 
-   * The function deleted the file, which is based in a paramter from the storage.
-   * The parameter should be point to the file, with the the full path without
-   * the part of the storage.
+   * Uses the system preference for the base storage to defined, where
+   * to the delete the file. The parameter path should point the file
+   * inside the base storage.
    * 
    * E.g.: Storage: '/abc'
    *       File to delete: '/abc/def/ghi.jpg'
@@ -190,27 +275,41 @@ export class StorageService extends BaseConfigService implements IStorageService
    * 
    * @public
    * 
-   * @param {string} path The full path to the file that should be deleted
+   * @param {string} path The path to the file that should be deleted
    * @returns {Promise<void>}
    * 
-   * @throws RequiredConfigParameterNotSetError
-   * @throws StorageNotExistsError
-   * @throws StorageNotWritableError
-   * @throws StorageFileNotExistingError
-   * @throws StorageFileNotDeletableError
+   * @throws {ServiceNotInitializedError}
+   * @throws {RequiredConfigParameterNotSetError}
+   * @throws {StorageFileNotExistingError}
+   * @throws {StorageFileNotDeletableError}
+   * @throws {Error}
    */
-  async deleteFile(path: string): Promise<void> {
-    await this.init();
-
+  public async deleteFile(path: string): Promise<void> {
     this.logger.log('Check if file exists on the filesystem', 'debug');
-    const file = this.baseStorage + '/' + path;
-    const exists = await this.exists(path);
-
-    if (!exists) {
-      const error = new StorageFileNotExistingError(file, 'File not existing');
+    
+    const baseStorage = await this.getBaseStorage();
+    if (!baseStorage) {
+      const error = new RequiredConfigParameterNotSetError(this.baseStorageKey, 'Storage must be defined to delete file');
+      this.logger.log('To delete file, the storage must be defined', 'error');
       this.logger.log(error.stack, 'error');
+
       throw error;
     }
+    
+    const file = baseStorage + '/' + path;
+
+    // Check if file exists in the storage
+    // If file, which should be deleted, is not available a error is thrown
+    try {
+      const exists = await fs.stat(file);
+    } catch (err) {
+      this.logger.log('File to delete, does not exist on the filesystem', 'debug');
+
+      const error = new StorageFileNotExistingError(file, 'File not existsing');
+      this.logger.log(error.stack, 'warn');
+      throw error;
+    }
+
 
     try {
       this.logger.log(`Now try to delete the file ${file} from the storage`, 'debug');
@@ -232,16 +331,20 @@ export class StorageService extends BaseConfigService implements IStorageService
    * 
    * @param {string} path The path to the file or directory, which should be checked
    * 
-   * @throws RequiredConfigParameterNotSetError
-   * @throws StorageNotExistsError
-   * @throws StorageNotWritableError 
+   * @throws {RequiredConfigParameterNotSetError}
+   * @throws {ServiceNotInitializedError}
+   * @throws {Error}
    */
-  async exists(path: string): Promise<boolean> {
-    await this.init();
-    
-    this.logger.log(`Service fully initalised. Now check if file or directory exists`, 'debug');
+  public async exists(path: string): Promise<boolean> {
+    const baseStorage = await this.getBaseStorage();
+    if (!baseStorage) {
+      const error = new RequiredConfigParameterNotSetError(this.baseStorageKey, 'Storage not defined');
+      this.logger.log(error.stack, 'error');
 
-    const fullPath = this.baseStorage + '/' + path;
+      throw error;
+    }
+    
+    const fullPath = baseStorage + '/' + path;
     
     try {
       await fs.stat(fullPath);
