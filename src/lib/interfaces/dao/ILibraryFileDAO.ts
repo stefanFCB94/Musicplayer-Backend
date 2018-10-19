ee6be4cfb557a9681dfbe7767487ede900218d38
stefanFCB94 <stefan.laeufle@gmail.com>
2018-10-19 21:30:38 +0200

    Updated interface

--- src/lib/interfaces/dao/ILibraryFileDAO.ts
+++ src/lib/interfaces/dao/ILibraryFileDAO.ts
@@ -1,3 +1,7 @@
+import { LibraryFile } from '../../db/models/LibraryFile';
+
 export interface ILibraryFileDAO {
-  
+  saveOrUpdateFile(file: LibraryFile): Promise<LibraryFile>;
+  getFileByChecksum(checksum: string): Promise<LibraryFile>;
+  deleteFile(file: LibraryFile): Promise<LibraryFile>;
 }
