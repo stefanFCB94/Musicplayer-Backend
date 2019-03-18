26cd74eb425dddeadaaa924d8195bfc62e4f59bc
Stefan Läufle <stefan.laeufle@gmail.com>
2019-03-18 22:58:56 +0100

    Added push command for logger microservice

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+
 docker build -t stefanfcb94/musicplayer:logger ./services/logger/service/. 
+docker push stefanfcb94/musicplayer:logger
